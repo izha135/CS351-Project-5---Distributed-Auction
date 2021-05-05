1270122e1a81d51d87e6201129a96f8cbdecc3b1
John Cooper <jfrancec@unm.edu>
2021-05-04 19:25:42 -0600

    AuctionHouse overhaul, cleaning it up to make it easier to modify

--- src/auctionHouse/AuctionHouse.java
+++ src/auctionHouse/AuctionHouse.java
@@ -1,58 +1,186 @@
 package auctionHouse;
 
-import common.BankAccount;
 import common.Item;
-import javafx.animation.Timeline;
+import common.MessageEnum;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.Serializable;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
-import java.io.*;
 import java.util.ArrayList;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.LinkedList;
+import java.util.List;
 
-public class AuctionHouse implements Serializable {
+public class AuctionHouse {
 
-    private String name;
-    private int publicID = 6000;
-    private String ahKey; // Requested and received from Auction Central
-    private HashMap<Integer, Item> items; //Item ID as key for the item.
-    private HashMap<Integer, TimerThread> itemTimers;
+    private static int port;
+    private static int ahId; // Requested and received from Auction Central
+    private static HashMap<Integer, Item> items; //Item ID as key for the item.
+    private static HashMap<Integer, TimerThread> itemTimers;
+    private static List<SocketInfo> userList;
 
     private int itemCounter = 0;
+    private static boolean run;
 
     private Item soldItem;
 
-
     /**
-     * AuctionHouse()
      * Creates an AuctionHouse that has three random items for sale.
      *
-     * @param name Name for this AuctionHouse
+     * args[0] is expected to be the hostname
      */
-    public AuctionHouse(String name) {
-        this.name = name;
+    public static void main(String[] args) {
         items = new HashMap<>();
-        //pendingHolds = new HashSet();
-        populateItems();
+        itemTimers = new HashMap<>();
+        userList = new LinkedList<>();
+        String hostName = args[0];
+        port = 3030;
+
+        String message;
+        String[] split;
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        AuctionHouseListener listener = new AuctionHouseListener(server, userList);
+        listener.start();
+
+        try (Socket socket = new Socket(hostName, port);
+             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+
+            writer.println(MessageEnum.HOUSE.toString());
+            while(!reader.ready()) System.out.print("");
+            //while(!reader.ready()) ;
+
+            // This is dealing with the login message from the Bank
+            message = reader.readLine();
+            split = message.split(";");
+            ahId = Integer.parseInt(split[1]);
+
+            // Get items for the auction house from the bank
+            populateItems(writer, reader);
+
+            // Now being listening and parsing messags
+            run = true;
+            while(run) {
+                for(int i = 0; i < userList.size(); i++) {
+                    if(userList.get(i).reader.ready()) {
+                        SocketInfo user = userList.get(i);
+                        message = user.reader.readLine();
+                        parseUserMessage(message, user.writer);
+                    }
+                }
+                if(reader.ready()) {
+                    message = reader.readLine();
+                    parseBankMessage(message, writer);
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // THE writer IS THE WRITER FOR THE USER. USE IT TO SEND MESSAGES
+    public static void parseUserMessage(String message, PrintWriter writer) {
+        String[] split = message.split(";");
+        MessageEnum command = MessageEnum.parseCommand(split[0]);
+
+        switch(command) {
+            case BID:
+                int userId = Integer.parseInt(split[1]);
+                double bidAmount = Double.parseDouble(split[2]);
+                int itemId = Integer.parseInt(split[3]);
+                int houseId = Integer.parseInt(split[4]); // Might wanna check if this agrees with the ahId
+                // Check if the item is in the AuctionHouse
+                // If not, send a REJECT message (formatted) to the user
+                // Else, send a VALID_BID message to the bank
+                break;
+            case GET_ITEM:
+                itemId = Integer.parseInt(split[1]);
+                // Return an ITEM message to the requesting user
+                break;
+            case GET_ITEMS:
+                // Return an ITEMS message to the requesting user
+                break;
+            case EXIT:
+                userId = Integer.parseInt(split[1]);
+                // Check if the user is the highest bidder on any of the items
+                // If true, send and ERROR message "Cannot exit"
+                // Else, return a CAN_EXIT message
+                break;
+            default:
+                System.out.println("Error: Unhandled user message");
+        }
+    }
+
+    public static void parseBankMessage(String message, PrintWriter writer) {
+        String[] split = message.split(";");
+        MessageEnum command = MessageEnum.parseCommand(split[0]);
+
+        switch(command) {
+            case ACCEPT_BID:
+                int userId = Integer.parseInt(split[1]);
+                int itemId = Integer.parseInt(split[2]);
+                // Change the highest bidder on the item to the userId in the message
+                // Send a ACCEPT message to the user
+                break;
+            case REJECT_BID:
+                userId = Integer.parseInt(split[1]);
+                itemId = Integer.parseInt(split[2]);
+                // Send a REJECT message to the user
+                break;
+            case CAN_EXIT:
+                // The bank says it is okay to exit
+                run = false;
+                // Do other things to exit gracefully
+                break;
+            case ERROR:
+                String errorMessage = split[1];
+                // Print the error message to the screen or something
+                break;
+            default:
+                System.out.println("Error: Unhandled bank message");
+        }
+    }
+
+    public void timerExpired(int itemId) {
+        // FIXME: deal with this
+        // To the highest bidder on the item with id itemId, send a WINNER message
+        // To the bank, send an AUCTION_ENDED message
+        // Remove the item associated with the itemId from the itemList
+        // and remove the item timer from the itemTimer list
     }
 
     /**
      * populateItems()
      * <p>
-     * This method fills the auctionHouse with 3 random items. A given item will be initialized with a -1 for it's
-     * ahID and will be initialized to an actual valid ahID after the auction house registers with auction central.
+     * This method fills the auctionHouse with 3 items from bank.
      */
-    public void populateItems() {
-        for (int i = 0; i < 3; ++i) {
-            // FIXME: implement getRandomItem() method first...
-//                Item item = ItemDB.getRandomItem();
-//                item.setItemId(itemCounter);
-//                items.put(itemCounter, item);
-//                itemCounter++;
+    public static void populateItems(PrintWriter writer, BufferedReader reader) {
+        writer.println(MessageEnum.GET_ITEMS_FROM_BANK + ";" + ahId + ";3");
+
+        try {
+            while(!reader.ready()) System.out.print("");
+            //while(!reader.ready()) ;
+
+            String message = reader.readLine();
+            String[] split = message.split(";");
+            // Split will have all the elements from a ITEMS list from the bank
+            // It can be checked that split[0] is ITEMS or not
+
+            for (int i = 0; i < 3; ++i) {
+                // TODO: Parse items from split. Put them in maps with key as the itemId
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -70,35 +198,6 @@ public class AuctionHouse implements Serializable {
         return output;
     }
 
-    public HashMap<Integer, Item> getItems() {
-        return items;
-    }
-
-    /**
-     * setIDs()
-     * <p>
-     * Sets the publicID and auction house key of this auction house.
-     *
-     * @param publicID ID given to AH from the auction central
-     * @param ahKey    Key given to aH from the auction central
-     */
-    public void setIDs(int publicID, String ahKey) {
-        this.publicID = publicID;
-        this.setAhKey(ahKey);
-        ArrayList<Item> itemsAsList = new ArrayList<Item>(items.values());
-        for (int i = 0; i < itemsAsList.size(); ++i) {
-            itemsAsList.get(i).setItemId(this.publicID);
-        }
-    }
-
-    public int getPublicID() {
-        return publicID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
     /**
      * placeBid()
      * Called by an Agent to place a bid (or by a Client when a PLACE_BID Message is received)
@@ -114,39 +213,17 @@ public class AuctionHouse implements Serializable {
      */
     public boolean placeBid(String biddingID, double amount, int itemID, int auctionHouseID) {
         //Safechecking
-        if (!(auctionHouseID == publicID)) {
-            //Not the right AuctionHouse USER OUTPUT
-            System.err.println(toString() + " received a placeBid request for auctionHouseID " + auctionHouseID + " which does" +
-                    "not match its public ID " + publicID + ". Returning.");
-            return false;
-        }
         Item item = items.get(itemID);
         if (item == null) {
             //That item isn't for sale here USER OUTPUT
-            System.err.println("Bidding ID " + biddingID + " tried to bid on " + itemID + ", which is not an item in " +
-                    name + ". Returning");
+            System.err.println("Bidding ID " + biddingID + " tried to bid on " + itemID + ", which is not an item in "
+                    + ". Returning");
             return false;
         }
 
-        //FIXME: some default return
-        return false;
+        //TODO: I don't know if this function will be useful - John
+        return true;
     }
-
-    public void timerExpired(int itemId) {
-        // FIXME: deal with this
-    }
-
-
-
-
-        /*public String processHoldResponse(String biddingID, double amount, int itemID, Timeline timer)
-        {
-            Item item = items.get(itemID);
-            String prevBidWinner = item.getCurrentHighestBidderID();
-            item.setCurrentBidAndBidder(amount, biddingID);
-            setBidTimer(itemID, timer);
-            return prevBidWinner;
-        }*/
 
     /**
      * @param itemID ID of the item stored in the timer that is called when the item is sold.
@@ -167,25 +244,7 @@ public class AuctionHouse implements Serializable {
         }*/
 
 
-    private void setSoldItem(Item soldItem) {
-        this.soldItem = soldItem;
-    }
-
-    public Item getSoldItem() {
-        // TODO: Set to null somewhere in logic of set/get/onfinished
-        return soldItem;
-    }
-
-    public String getAhKey() {
-        return ahKey;
-    }
-
-    public void setAhKey(String ahKey) {
-        this.ahKey = ahKey;
-    }
-
-
-    private class PendingBidRequest {
+    /*private class PendingBidRequest {
         public final String BIDDING_ID;
         public final double AMOUNT;
         public final int ITEM_ID;
@@ -205,76 +264,8 @@ public class AuctionHouse implements Serializable {
         }
 
         //TODO: override hash to use in Set.
-    }
+    }*/
 
-    /**
-     * itemDB inner Class
-     * <p>
-     * Reads a static file to populate items into an array.
-     * Has a method getRandomItem() to grab a random copy of one of these items.
-     * This design is opposed to hosting a real SQL database and sending updates to it. Instead we have a file.
-     */
-    private static class ItemDB {
-        private static ArrayList<Item> items;
-
-        // Static initializer that always loads the filelist
-        static {
-            items = new ArrayList<>();
-
-            try {
-                InputStream inputFile = ItemDB.class.getResourceAsStream("ItemList.txt");
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputFile, "UTF-8"));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // This allows for commenting in the ItemList
-                    if (!line.startsWith("//") && !line.trim().isEmpty()) {
-                        // The ItemList is fragile, be careful editing it.
-                        // ***Changed some things (like itemID from imgPath)
-                        String[] elements = line.split(",");
-                        String itemName = elements[0];
-                        //String imgPath = elements[1];
-                        int itemID = Integer.valueOf(elements[1]);
-                        //Double minimumBid = Double.valueOf(elements[2]);
-                        int minimumBid = Integer.valueOf(elements[2]);
-                        String itemDesc = elements[3];
-                        items.add(new Item(itemName, itemID, minimumBid,
-                                itemDesc));
-                    }
-                }
-                inputFile.close();
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-
-//            /**
-//             * getRandomItem()
-//             *
-//             * Picks a random item from the itemList gotten from ItemList.txt and then returns a copy of it.
-//             * If it were to return the actual memory object then we would have comparison conflicts elsewhere in
-//             * the code base. Item name's don't have to be unique but their memory addresses have to be.
-//             *
-//             * @return Copy of a random item
-//             */
-//            private static Item getRandomItem()
-//            {
-//                return new Item(items.get(ThreadLocalRandom.current().nextInt(0, items.size())));
-//            }
-    }
-
-    /* try {
-         Socket socket = new Socket(hostName, port);
-     //     PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-     //     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));){
-
-     PrintWriter writer = new PrintWriter(System.out, true);
-     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-     }
-     catch (Exception e) {
-       e.printStackTrace();
-     }
-*/
     static class SocketInfo {
         final Socket socket;
         final PrintWriter writer;
@@ -287,5 +278,3 @@ public class AuctionHouse implements Serializable {
         }
     }
 }
-
-
