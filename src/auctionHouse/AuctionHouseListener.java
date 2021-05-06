a842383dc651fd93825a6c724565fa9c15c27d46
John Cooper <jfrancec@unm.edu>
2021-05-06 07:24:27 -0600

    Some debugging work with the User and Bank

--- src/auctionHouse/AuctionHouseListener.java
+++ src/auctionHouse/AuctionHouseListener.java
@@ -25,8 +25,12 @@ public class AuctionHouseListener extends Thread{
     public void run() {
         while(run) {
             try {
+                //System.out.println(server);
                 Socket socket = server.accept();
                 System.out.println("Accepted user socket");
+                System.out.println("Socket Inet Address Host name: " + socket.getInetAddress().getHostName());
+                System.out.println("Socket Inet Address Host address: " + socket.getInetAddress().getHostAddress());
+                System.out.println(socket.toString());
 
                 InputStream input = socket.getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(input));
@@ -34,10 +38,8 @@ public class AuctionHouseListener extends Thread{
                 OutputStream output = socket.getOutputStream();
                 PrintWriter writer = new PrintWriter(output, true);
 
-                while (!reader.ready()) ;
-                //while (!reader.ready()) System.out.print("");
-
                 String line = reader.readLine();
+                System.out.println(line);
                 String[] split = line.split(";");
                 // Test if the message indicates a house
                 int userId = Integer.parseInt(split[0]);
@@ -50,5 +52,9 @@ public class AuctionHouseListener extends Thread{
                 Thread.currentThread().interrupt();
             }
         }
+    }
+
+    public synchronized void stopRunning() {
+        run = false;
     }
 }
--- src/auctionHouse/TimerThread.java
+++ src/auctionHouse/TimerThread.java
@@ -6,13 +6,13 @@ public class TimerThread extends Thread{
     private int itemId;
 
     public TimerThread(int itemId) {
-        startTime = System.nanoTime();
         this.itemId = itemId;
     }
 
     @Override
     public void run() {
         boolean run = true;
+        startTime = System.nanoTime();
         while(run) {
             if((System.nanoTime() - startTime) > DURATION * 1_000_000_000) {
                 run = false;
